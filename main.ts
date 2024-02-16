306038b6a5885d557510d9dba7009d48a242551a
Paulo Vanderley <vanderley.1109@gmail.com>
2024-02-16 15:19:53 -0300

    adicionando conteudo

---
+++ main.ts
@@ -0,0 +1,27 @@
+// Declaração de variáveis numéricas.
+let num1: number = 5;
+let num2: number = 3;
+
+// Operação de soma
+let soma: number = num1 + num2;
+console.log(soma); // Saída: 8.
+
+// Operação de subtração.
+let subtracao: number = num1 - num2;
+console.log(subtracao); // Saída: 2.
+
+// Operação de multiplicação.
+let multiplicacao: number = num1 * num2;
+console.log(multiplicacao); // Saída: 15.
+
+// Operação de divisão.
+let divisao: number = num1 / num2;
+console.log(divisao); // Saída: 1.6666666666666667.
+
+// Operação de módulo (resto da divisão).
+let modulo: number = num1 % num2;
+console.log(modulo); // Saída: 2.
+
+// Concatenação de strings.
+let concatenacao: string = "Paulo" + " " + "Vanderley";
+console.log(concatenacao); // Saída: "Paulo Vanderley".
